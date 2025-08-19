7f84a1727b5095c5039d86a71a65e2cb0562490d
Dhanashree Raut <daraut0203@gmail.com>
2025-08-19 23:51:29 +0530

    Sound added

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ var buttonLength = document.querySelectorAll("button").length ;
 for( var i =0 ; i< buttonLength ; i++ ){
     document.querySelectorAll("button")[i].addEventListener("click" , 
         function (){
-            alert("Clicked");
+            // alert("Clicked");
+            
+
+            let buttonSound = {
+                'w': 'sounds/tom-1.mp3',
+                'a': 'sounds/tom-2.mp3',
+                's': 'sounds/tom-3.mp3',
+                'd': 'sounds/tom-4.mp3',
+                'j': 'sounds/snare.mp3',
+                'k': 'sounds/crash.mp3',
+                'l': 'sounds/kick-bass.mp3',
+            };
+
+
+            button = this.innerHTML;
+            var audio = new Audio(buttonSound[button]);
+            audio.play();
+
+            // for( key in buttonSound){
+            //     console.log(key);
+            //     console.log(buttonSound[key]);
+            // }
+
+           
+            // console.log(buttonSound);
     });
 }
